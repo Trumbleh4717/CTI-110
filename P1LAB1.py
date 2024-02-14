6617a0fcf263f64a8b459ade707836acc8b24bd3
Trumbleh4717 <160040796+Trumbleh4717@users.noreply.github.com>
2024-02-14 10:34:12 -0500

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,12 @@
+#Hailey Trumble
+#2/12/2024
+#PILAB1
+#Using input and output
+
+#Create a variable to hold Hailey
+Name =input ("Enter your name")
+
+#Print Hailey with greeting
+print("Hey",Name)
+
+print("Welcome to CTI-110")
